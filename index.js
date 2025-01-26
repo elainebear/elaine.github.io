1bcfb34445400167db2f940f04baea1e14f5fc93
Elaine <q7470251@gmail.com>
2025-01-26 12:55:43 +0800

    update html id naming and improve js code

--- index.js
+++ index.js
@@ -1,66 +1,42 @@
-/*載入畫面*/
 $(window).load(function() { // 確認整個頁面讀取完畢再將這三個div隱藏起來
     $("#status").delay(0).fadeOut(1000); //delay --> 延遲幾秒才fadeOut
     $("#preloader").delay(250).fadeOut(1000);
 })
 
 
-/*導覽列變色*/
+
 $(document).ready(() => {
-            //滾動事件
-	$(document).scroll(() => {
-		let Nav = $('nav').offset().top;
-		let header = $('header').offset().top;
-		let item1 = $('article#About').offset().top; 
-		let item2 = $('article#Works').offset().top; 
-		//let item3 = $('article#Project').offset().top;
-		//let item4 = $('article#Publications').offset().top;
-		let item5 = $('article#Contact').offset().top; 
-		
-		
-		if(Nav > header){ //當 nav 滾到 item1 的區域內
-			$("li#item1 a").css({'color':'#000',});
-			$("li#item2 a").css({'color':'#000',});
-			//$("li#item3 a").css({'color':'#000',});
-			//$("li#item4 a").css({'color':'#000',});
-			$("li#item5 a").css({'color':'#000',});
-		}
-		if(Nav > (item1-350)){ //當 nav 滾到 item1 的區域內
-			$("li#item1").css({'background-image': 'linear-gradient(45deg, #F37335, #FDC830 80%)',
-			'-webkit-background-clip': 'text', /* 背景裁剪成文字的前景色 */
-			'background-clip': 'text',
+    const sections = [];
+	$("article").each(function() {
+		const id = $(this).attr("id");
+		sections.push({
+			id: id,
+			offsetTop: $(this).offset().top,
+			navItem: $(`li#item_${id}`)
+		});
+	});
+
+	$(document).scroll(()=>{
+		const scrollpos = $(document).scrollTop();
+		let activesection = null;
+		sections.forEach((section)=>{
+			if(scrollpos >= (section.offsetTop-350)){
+				activesection = section;
+			}
+		})
+
+		$("nav li").css({'background-color':'none'});
+		$("nav li a").css({'color':'#000'});
+
+		if(activesection){
+			activesection.navItem.css({
+				'background-image': 'linear-gradient(45deg, #F37335, #FDC830 80%)',
+				'-webkit-background-clip': 'text', 
+				'background-clip': 'text',
 			});
-			$("li#item1 a").css({'color':'transparent',});		
-			$("li#item2 a").css({'color':'#000',});
-			//$("li#item3 a").css({'color':'#000',});
-			//$("li#item4 a").css({'color':'#000',});
-			$("li#item5 a").css({'color':'#000',});
+			activesection.navItem.find('a').css({'color': 'transparent'});
 		}
-		if(Nav > (item2-350)){ //當 nav 滾到 area02 的區域內
-			$("li#item2").css({'background-image': 'linear-gradient(45deg, #F37335, #FDC830 80%)',
-			'-webkit-background-clip': 'text', /* 背景裁剪成文字的前景色 */
-			'background-clip': 'text',
-			});
-			$("li#item2 a").css({'color':'transparent',});
-			$("li#item1 a").css({'color':'#000',});
-			//$("li#item3 a").css({'color':'#000',});
-			//$("li#item4 a").css({'color':'#000',});
-			$("li#item5 a").css({'color':'#000',});	
-		}
-		
-		
-		if(Nav > (item5-350)){ //當 nav 滾到 area02 的區域內
-			$("li#item5").css({'background-image': 'linear-gradient(45deg, #F37335, #FDC830 80%)',
-			'-webkit-background-clip': 'text', /* 背景裁剪成文字的前景色 */
-			'background-clip': 'text',
-			});
-			$("li#item5 a").css({'color':'transparent',});
-			$("li#item1 a").css({'color':'#000',});
-			$("li#item2 a").css({'color':'#000',});
-			//$("li#item3 a").css({'color':'#000',});
-			//$("li#item4 a").css({'color':'#000',});	
-		}
-  });
+
+	});
+
 });
-	
- 
