54999e45e0d36c7f9313bfec635abeeec8cdae9f
Elaine <q7470251@gmail.com>
2025-04-23 15:28:36 +0800

    Improve js readability

--- index.js
+++ index.js
@@ -1,42 +1,48 @@
-$(window).load(function() { // 確認整個頁面讀取完畢再將這三個div隱藏起來
-    $("#status").delay(0).fadeOut(1000); //delay --> 延遲幾秒才fadeOut
-    $("#preloader").delay(250).fadeOut(1000);
+$(window).on('load',function() { // 確認整個頁面讀取完畢再將這三個div隱藏起來
+    $("#status").fadeOut(1000); 
+    $("#preloader").delay(250).fadeOut(1000); //delay --> 延遲幾秒才fadeOut
 })
 
 
 
 $(document).ready(() => {
-    const sections = [];
-	$("article").each(function() {
-		const id = $(this).attr("id");
-		sections.push({
-			id: id,
-			offsetTop: $(this).offset().top,
-			navItem: $(`li#item_${id}`)
-		});
-	});
+    // const sections = [];
+	// $("article").each(function() {
+	// 	// const id = $(this).attr("id");
+	// 	sections.push({
+	// 		id: this.id,
+	// 		offsetTop: $(this).offset().top,
+	// 		navItem: $(`li#item_${id}`)
+	// 	});
+	// });
+	const sections = $("article").map(function() {
+        return {
+            id: this.id,
+            navItem: $(`li#item_${this.id}`)
+        };
+    }).get();
 
-	$(document).scroll(()=>{
+	$(document).on('scroll', ()=>{
 		const scrollpos = $(document).scrollTop();
 		let activesection = null;
-		sections.forEach((section)=>{
-			if(scrollpos >= (section.offsetTop-350)){
-				activesection = section;
-			}
-		})
 
-		$("nav li").css({'background-color':'none'});
-		$("nav li a").css({'color':'#000'});
+		// sections.forEach((section)=>{
+		// 	if(scrollpos >= (section.offsetTop-350)){
+		// 		activesection = section;
+		// 	}
+		// })
+		for (let i = sections.length - 1; i >= 0; i--) {
+            const offsetTop = $(`#${sections[i].id}`).offset().top;
+            if (scrollpos >= offsetTop - 350) {
+                activesection = sections[i];
+                break;
+            }
+        }
 
+		$("nav li").removeClass("active");
 		if(activesection){
-			activesection.navItem.css({
-				'background-image': 'linear-gradient(45deg, #F37335, #FDC830 80%)',
-				'-webkit-background-clip': 'text', 
-				'background-clip': 'text',
-			});
-			activesection.navItem.find('a').css({'color': 'transparent'});
+			activesection.navItem.addClass("active");
 		}
-
 	});
 
 });
